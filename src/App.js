17f391b6bebf56cd85b88dac76e1fdad8122cd99
uabdirasulm <ubbiniyazov04@gmail.com>
2023-05-03 14:31:21 +0500

    add many cards and styles

--- src/App.js
+++ src/App.js
@@ -13,6 +13,38 @@ class App extends Component {
           price="4.99"
           off="30"
         />
+        <Card
+          title="Orange Juice"
+          img="https://www.minutemaid.com/content/dam/nagbrands/us/minutemaidus/en/products/variety-juices-and-more/jtg/JTG-PCP-Thumbnails-orange.png"
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Reiciendis, sint ad voluptatem doloribus labore voluptas?"
+          price="3.49"
+          off=""
+        />
+        <Card
+          title="Cherry Juice"
+          img="https://www.minutemaid.com/content/dam/nagbrands/us/minutemaidus/en/products/variety-juices-and-more/jtg/JTG-PCP-Thumbnails-orange.png"
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Reiciendis, sint ad voluptatem doloribus labore voluptas?"
+          price="9.99"
+          off="30"
+        />
+        <Card
+          title="Grape Juice"
+          img="https://www.minutemaid.com/content/dam/nagbrands/us/minutemaidus/en/products/variety-juices-and-more/jtg/JTG-PCP-Thumbnails-orange.png"
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+    Reiciendis, sint ad voluptatem doloribus labore voluptas?"
+          price="2.99"
+          off="10"
+        />
+        <Card
+          title="Apple Juice"
+          img="https://www.minutemaid.com/content/dam/nagbrands/us/minutemaidus/en/products/variety-juices-and-more/jtg/JTG-PCP-Thumbnails-orange.png"
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Reiciendis, sint ad voluptatem doloribus labore voluptas?"
+          price="4.99"
+          off="60"
+        />
       </div>
     );
   }
